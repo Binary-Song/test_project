02090d4e6930040931ac921a5555e102f849b4af
O5-11 <39667508+Binary-Song@users.noreply.github.com>
2021-02-20 12:39:21 +0800

    Update DailyCP.py

--- DailyCP.py
+++ DailyCP.py
@@ -278,6 +278,7 @@ class DailyCP:
                     file.write(json.dumps(
                         form, ensure_ascii=False).encode("utf-8"))
                     print("请手动填写{formpath}，之后重新运行脚本".format(formpath=formpath))
+                    print("未知的表单")
                     raise
 
         confirmList = self.getNoticeList()
